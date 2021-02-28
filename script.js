ce9624f32ac92a9792e821b6a8b508929e28cb60
Jspena01 <josesamuelrd@gmail.com>
2021-02-28 19:21:46 +0100

    Proyecto convalición de formulario

---
+++ script.js
@@ -0,0 +1,44 @@
+
+const nombre = document.getElementById("nombre");
+const materias = document.getElementById("materias"); 
+const email = document.getElementById("email")
+const error = document.getElementById("error");
+
+validarInfo = () =>{
+ if(nombre.value == ""){
+     error.textContent = " Ingrese su nombre";
+     error.classList.add("fas","fa-times");
+}else if(materias.value == ""){
+    error.textContent = " Complete el campo materias";
+    error.classList.add("fas","fa-times");
+
+ }else if(email.value == "" && !email.value.includes("@")){
+    error.textContent = " Ingrese su correo electronico";
+    error.classList.add("fas","fa-times");
+
+ }else if( email.value != "" && !email.value.includes("@")){
+    error.textContent = " Ingrese su correo electronico correctamente";
+    error.classList.add("fas","fa-times");
+ }else if(!email.value.substring(email.value.indexOf("@"), email.value.Length).includes(".")){
+    error.textContent = " Ingrese su correo electronico correctamente";
+    error.classList.add("fas","fa-times");
+ }else if(email.value.substring(email.value.indexOf("@"), email.value.Length).includes(".") && !(email.value.substring(email.value.indexOf("."), email.value.Length).length > 1)){
+    error.textContent = " Ingrese su correo electronico correctamente";
+    error.classList.add("fas","fa-times");
+ }
+ else{
+     error.textContent = "";
+     error.classList.remove("fas","fa-times");
+     alert("Su solicitud a sido enviado");
+    nombre.value = "";
+    materias.value = "";
+    email.value = "";
+ }
+
+}
+
+
+enviar.addEventListener("click",(e)=>{
+    e.preventDefault();
+    validarInfo();
+})
