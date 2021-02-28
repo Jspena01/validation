aaf4ab612a3e8255adb052e5ed653abcaffc250c
Jspena01 <josesamuelrd@gmail.com>
2021-02-28 20:41:54 +0100

    Modificación condición de validaciones

--- script.js
+++ script.js
@@ -5,18 +5,18 @@ const email = document.getElementById("email")
 const error = document.getElementById("error");
 
 validarInfo = () =>{
- if(nombre.value == ""){
+ if(nombre.value == "" || nombre.value != "" && !(nombre.value.length >= 5)){
      error.textContent = " Ingrese su nombre";
      error.classList.add("fas","fa-times");
-}else if(materias.value == ""){
+}else if(materias.value == "" || materias.value != "" && !(materias.value.length >= 5)){
     error.textContent = " Complete el campo materias";
     error.classList.add("fas","fa-times");
 
- }else if(email.value == "" && !email.value.includes("@")){
+ }else if(email.value == "" || email.value != "" && !(email.value.length >= 5)){
     error.textContent = " Ingrese su correo electronico";
     error.classList.add("fas","fa-times");
 
- }else if( email.value != "" && !email.value.includes("@")){
+ }else if( email.value != "" && !email.value.includes("@") && nombre.value.length > 6){
     error.textContent = " Ingrese su correo electronico correctamente";
     error.classList.add("fas","fa-times");
  }else if(!email.value.substring(email.value.indexOf("@"), email.value.Length).includes(".")){
